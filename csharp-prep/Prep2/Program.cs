10e9683323c8464294f0551fc6c3ce39a24ba502
duncan <duncan@smarty.com>
2025-01-10 23:24:23 -0700

    Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your grade percentage: ");
+        string rawGrade = Console.ReadLine();
+
+        int grade = int.Parse(rawGrade);
+
+        string letter;
+        if (grade > 89)
+        {
+            letter = "A";
+        }
+        else if (grade > 79)
+        {
+            letter = "B";
+        }
+        else if (grade > 69)
+        {
+            letter = "C";
+        }
+        else if (grade > 59)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"You got a(n) {letter}");
+
+        if (grade > 69)
+        {
+            Console.WriteLine("Congratulations!");
+        }
+        else
+        {
+            Console.WriteLine("You'll do better next time!");
+        }
     }
 }
